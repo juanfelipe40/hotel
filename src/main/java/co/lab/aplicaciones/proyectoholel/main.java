748266cd9fbd4411baf738b2e0fd5ae6849c3357
sala401 <sala401@SALA401_WS07.IUSH.LOCAL>
2016-08-30 08:30:31 -0500

    30 de agosto 2

--- src/main/java/co/lab/aplicaciones/proyectoholel/main.java
+++ src/main/java/co/lab/aplicaciones/proyectoholel/main.java
@@ -28,6 +28,11 @@ public class main {
       hotel.setLocalidad("Medellin"); 
       
       hotelDao.save(hotel);
+//      hotelDao.save(hotel);
+//      hotelDao.save(hotel);
+//      hotelDao.save(hotel);
+//      hotelDao.save(hotel);
+      
     }
 
 }
