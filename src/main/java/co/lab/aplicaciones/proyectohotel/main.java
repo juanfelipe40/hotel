1be2f877503bbf215fef726f1706fbe10200d7f6
usuario <usuario@192.168.1.10>
2016-08-30 17:48:25 -0500

    Este es el bueno

---
+++ src/main/java/co/lab/aplicaciones/proyectohotel/main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.lab.aplicaciones.proyectohotel;
+
+import co.lab.aplicaciones.proyectohotel.modelo.Hotel;
+import co.lab.aplicaciones.proyectohotel.repository.HotelDao;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Esteban
+ */
+public class main {
+    public static void main(String[] args) {
+      ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+      
+      HotelDao hotelDao = context.getBean(HotelDao.class);
+      
+      Hotel hotel= new Hotel();
+      
+      hotel.setNombre("El Balcon");
+      hotel.setDireccion("Calle Bolivar");
+      hotel.setCategoria("Categoria 1");
+      hotel.setLocalidad("Medellin"); 
+      
+      hotelDao.save(hotel);
+
+    }
+
+}
